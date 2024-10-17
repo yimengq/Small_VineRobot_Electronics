61a1c530b5a5b55e2e6ce62794afea95bc1d0272
kjwtu <kjwtu@stanford.edu>
2024-10-17 13:44:48 -0700

    Added software for both esps

---
+++ Software/Arduino/FULLSINGLE_Test/FULLSINGLE_Test.ino
@@ -0,0 +1,309 @@
+#include "esp_camera.h"
+#include <WiFi.h>
+#include "esp_http_server.h"
+#include "esp_timer.h"
+#include "img_converters.h"
+#include "fb_gfx.h"
+#include "esp32-hal-ledc.h"
+#include "sdkconfig.h"
+#include "imu_function.h"
+
+const int ledPin = D0;
+int ledState = 0;
+
+//Replace with your network credentials
+const char* ssid = "Vcc-AP";
+const char* password = "12345678";
+
+imu_function imu_func;
+
+#define CAMERA_MODEL_XIAO_ESP32S3 // Has PSRAM
+
+#if defined(CAMERA_MODEL_XIAO_ESP32S3)
+  #define PWDN_GPIO_NUM  -1
+  #define RESET_GPIO_NUM -1
+  #define XCLK_GPIO_NUM  10
+  #define SIOD_GPIO_NUM  40
+  #define SIOC_GPIO_NUM  39
+
+  #define Y9_GPIO_NUM    48
+  #define Y8_GPIO_NUM    11
+  #define Y7_GPIO_NUM    12
+  #define Y6_GPIO_NUM    14
+  #define Y5_GPIO_NUM    16
+  #define Y4_GPIO_NUM    18
+  #define Y3_GPIO_NUM    17
+  #define Y2_GPIO_NUM    15
+  #define VSYNC_GPIO_NUM 38
+  #define HREF_GPIO_NUM  47
+  #define PCLK_GPIO_NUM  13
+#else
+  #error "Camera model not selected"
+#endif
+
+typedef struct {
+  size_t size;   //number of values used for filtering
+  size_t index;  //current value index
+  size_t count;  //value count
+  int sum;
+  int *values;  //array to be filled with values
+} ra_filter_t;
+
+static ra_filter_t ra_filter;
+
+static ra_filter_t *ra_filter_init(ra_filter_t *filter, size_t sample_size) {
+  memset(filter, 0, sizeof(ra_filter_t));
+
+  filter->values = (int *)malloc(sample_size * sizeof(int));
+  if (!filter->values) {
+    return NULL;
+  }
+  memset(filter->values, 0, sample_size * sizeof(int));
+
+  filter->size = sample_size;
+  return filter;
+}
+
+static int ra_filter_run(ra_filter_t *filter, int value) {
+  if (!filter->values) {
+    return value;
+  }
+  filter->sum -= filter->values[filter->index];
+  filter->values[filter->index] = value;
+  filter->sum += filter->values[filter->index];
+  filter->index++;
+  filter->index = filter->index % filter->size;
+  if (filter->count < filter->size) {
+    filter->count++;
+  }
+  return filter->sum / filter->count;
+}
+
+#define PART_BOUNDARY "123456789000000000000987654321"
+static const char *_STREAM_CONTENT_TYPE = "multipart/x-mixed-replace;boundary=" PART_BOUNDARY;
+static const char *_STREAM_BOUNDARY = "\r\n--" PART_BOUNDARY "\r\n";
+static const char *_STREAM_PART = "Content-Type: image/jpeg\r\nContent-Length: %u\r\nX-Timestamp: %d.%06d\r\n\r\n";
+
+httpd_handle_t stream_httpd = NULL;
+
+static esp_err_t stream_handler(httpd_req_t *req){
+  camera_fb_t * fb = NULL;
+  struct timeval _timestamp;
+  esp_err_t res = ESP_OK;
+  size_t _jpg_buf_len = 0;
+  uint8_t * _jpg_buf = NULL;
+  char * part_buf[128];
+
+  static int64_t last_frame = 0;
+  if (!last_frame) {
+    last_frame = esp_timer_get_time();
+  }
+
+  res = httpd_resp_set_type(req, _STREAM_CONTENT_TYPE);
+  if(res != ESP_OK){
+    return res;
+  }
+
+  httpd_resp_set_hdr(req, "Access-Control-Allow-Origin", "*");
+  httpd_resp_set_hdr(req, "X-Framerate", "5");
+
+  while(true){
+    fb = esp_camera_fb_get();
+    if (!fb) {
+      Serial.println("Camera capture failed");
+      res = ESP_FAIL;
+    } else {
+      _timestamp.tv_sec = fb->timestamp.tv_sec;
+      _timestamp.tv_usec = fb->timestamp.tv_usec;
+      if(fb->width > 400){
+        if(fb->format != PIXFORMAT_JPEG){
+          bool jpeg_converted = frame2jpg(fb, 80, &_jpg_buf, &_jpg_buf_len);
+          esp_camera_fb_return(fb);
+          fb = NULL;
+          if(!jpeg_converted){
+            Serial.println("JPEG compression failed");
+            res = ESP_FAIL;
+          }
+        } else {
+          _jpg_buf_len = fb->len;
+          _jpg_buf = fb->buf;
+        }
+      }
+    }
+
+    if (res == ESP_OK) {
+      res = httpd_resp_send_chunk(req, _STREAM_BOUNDARY, strlen(_STREAM_BOUNDARY));
+    }
+    if (res == ESP_OK) {
+      size_t hlen = snprintf((char *)part_buf, 128, _STREAM_PART, _jpg_buf_len, _timestamp.tv_sec, _timestamp.tv_usec);
+      res = httpd_resp_send_chunk(req, (const char *)part_buf, hlen);
+    }
+    if (res == ESP_OK) {
+      res = httpd_resp_send_chunk(req, (const char *)_jpg_buf, _jpg_buf_len);
+    }
+
+    if(fb){
+      esp_camera_fb_return(fb);
+      fb = NULL;
+      _jpg_buf = NULL;
+    } else if(_jpg_buf){
+      free(_jpg_buf);
+      _jpg_buf = NULL;
+    }
+    if(res != ESP_OK){
+      log_e("Send frame failed");
+      break;
+    }
+    
+    
+    int64_t fr_end = esp_timer_get_time();
+
+    int64_t frame_time = fr_end - last_frame;
+    frame_time /= 1000;
+    uint32_t avg_frame_time = ra_filter_run(&ra_filter, frame_time);
+    log_i(
+      "MJPG: %uB %ums (%.1ffps), AVG: %ums (%.1ffps)",
+      (uint32_t)(_jpg_buf_len), (uint32_t)frame_time, 1000.0 / (uint32_t)frame_time, avg_frame_time, 1000.0 / avg_frame_time
+    );
+
+  }
+  return res;
+}
+
+void startCameraServer(){
+  httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+  config.server_port = 80;
+
+  httpd_uri_t stream_uri = {
+    .uri = "/",
+    .method = HTTP_GET,
+    .handler = stream_handler,
+    .user_ctx = NULL
+#ifdef CONFIG_HTTPD_WS_SUPPORT
+    ,
+    .is_websocket = true,
+    .handle_ws_control_frames = false,
+    .supported_subprotocol = NULL
+#endif
+  };
+
+  ra_filter_init(&ra_filter, 20);
+  
+  log_i("Starting stream server on port: '%d'", config.server_port);
+  if (httpd_start(&stream_httpd, &config) == ESP_OK) {
+    httpd_register_uri_handler(stream_httpd, &stream_uri);
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial) delay(10);  // wait for serial port to open!
+  Serial.setDebugOutput(true);
+  Serial.println();
+
+  Serial.println("Full Single ESP32 Test"); Serial.println("");
+
+  imu_func.init();
+
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(1000);
+  
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  // config.frame_size = FRAMESIZE_UXGA;
+  config.frame_size = FRAMESIZE_HD;
+  config.pixel_format = PIXFORMAT_JPEG;  // for streaming
+  //config.pixel_format = PIXFORMAT_RGB565; // for face detection/recognition
+  config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
+  config.fb_location = CAMERA_FB_IN_PSRAM;
+  config.jpeg_quality = 12;
+  config.fb_count = 1;
+  
+  // if PSRAM IC present, init with UXGA resolution and higher JPEG quality
+  //                      for larger pre-allocated frame buffer.
+  if (config.pixel_format == PIXFORMAT_JPEG) {
+    if (psramFound()) {
+      config.jpeg_quality = 10;
+      config.fb_count = 2;
+      config.grab_mode = CAMERA_GRAB_LATEST;
+    } else {
+      // Limit the frame size when PSRAM is not available
+      config.frame_size = FRAMESIZE_SVGA;
+      config.fb_location = CAMERA_FB_IN_DRAM;
+    }
+  } else {
+    // Best option for face detection/recognition
+    config.frame_size = FRAMESIZE_240X240;
+#if CONFIG_IDF_TARGET_ESP32S3
+    config.fb_count = 2;
+#endif
+  }
+  
+  // Camera init
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+
+  sensor_t *s = esp_camera_sensor_get();
+  // initial sensors are flipped vertically and colors are a bit saturated
+  if (s->id.PID == OV3660_PID) {
+    s->set_vflip(s, 1);        // flip it back
+    s->set_brightness(s, 1);   // up the brightness just a bit
+    s->set_saturation(s, -2);  // lower the saturation
+  }
+  // // drop down frame size for higher initial frame rate
+  // if (config.pixel_format == PIXFORMAT_JPEG) {
+  //   s->set_framesize(s, FRAMESIZE_QVGA);
+  // }
+
+  // Wi-Fi connection
+  WiFi.begin(ssid, password);
+  WiFi.setSleep(false);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  
+  startCameraServer();
+
+  Serial.print("Camera Ready! Use 'http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("' to connect");
+}
+
+void loop() {
+  imu_func.update();
+  delay(1);
+  digitalWrite(ledPin, ledState);  // turn the LED on (HIGH is the voltage level)
+  ledState = ~ledState;
+
+  Serial.print("Camera Ready! Use 'http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("' to connect");
+}
+
+
