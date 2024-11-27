72872144ea53b58d9fedc7512c85d4d076c4fce7
kjwtu <kjwtu@stanford.edu>
2024-11-26 21:19:56 -0800

    Merged IMU, LED, and Cam code

---
+++ Software/Arduino/CameraWebServer/imu_function.h
@@ -0,0 +1,28 @@
+
+#ifndef __IMU_FUNCTION_H__
+#define __IMU_FUNCTION_H__
+
+class imu_function{
+    public:
+    void init();
+
+    void update();
+};
+
+#endif /* __IMU_FUNCTION_H__ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
