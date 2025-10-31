42c45d89f0659b1e1465ae5b5032cf3977613bd9
radxa <elvyyao@stanford.edu>
2025-10-31 22:20:54 +0000

    updating

--- data_out.py
+++ data_out.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import subprocess
-from vpython import box, vector, rate, radians
 import math
 import socket
 import time
@@ -9,30 +8,7 @@ import time
 HOST = "192.168.1.17"  # ← Replace with your Mac’s IP
 PORT = 6000
 port_command = "ls /dev/ttyACM*"
-path_command ="/home/radxa/Small_VineRobot_Electronics/inertial-sense-sdk/ExampleProjects/ISComm/build/"
-
-# def set_orientation(roll, pitch, yaw):
-#     # Convert to radians
-#     roll = radians(roll)
-#     pitch = radians(pitch)
-#     yaw = radians(yaw)
-
-#     # Direction vectors based on yaw-pitch-roll rotation order (Z-Y-X)
-#     # Rotation matrices combined manually:
-#     x_axis = vector(
-#         math.cos(yaw)*math.cos(pitch),
-#         math.sin(yaw)*math.cos(pitch),
-#         -math.sin(pitch)
-#     )
-#     y_axis = vector(
-#         math.cos(yaw)*math.sin(pitch)*math.sin(roll) - math.sin(yaw)*math.cos(roll),
-#         math.sin(yaw)*math.sin(pitch)*math.sin(roll) + math.cos(yaw)*math.cos(roll),
-#         math.cos(pitch)*math.sin(roll)
-#     )
-    
-#     cube.axis = x_axis      # "axis" defines the cube's forward direction
-#     cube.up = y_axis        # "up" defines the cube's upward direction
-
+path_command ="/home/radxa/Small_VineRobot_Electronics/"
 
 # finding correct port (becuase it seems to change for some reason)
 ports = subprocess.getoutput(port_command)
@@ -51,23 +27,23 @@ process = subprocess.Popen(
 )
 
 #socket time 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
-print("Connected to server")
+# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# s.connect((HOST, PORT))
+# print("Connected to server")
 
 # cube = box(color=vector(0,0,1))
 while True:
     line = process.stdout.readline()
-    # print(line)
+    print(line)
     # print time as well probably
     
     #for future use 
-    euler_list= line.split()
-    roll = euler_list[0]
-    pitch =euler_list[1]
-    yaw = euler_list[2]
+    # euler_list= line.split()
+    # roll = euler_list[0]
+    # pitch =euler_list[1]
+    # yaw = euler_list[2]
     
-    s.sendall(line.encode())
+    # s.sendall(line.encode())
 
 # if KeyboardInterrupt:
 #     print("done")
